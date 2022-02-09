ca5b26e46ea4cda9954e92832043c342c939b24c
董阔 <dongkuo@vzan.com>
2022-02-09 09:57:03 +0800

    优化环境变量与启动参数

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const compress = require('koa-compress')
 const state = require('./app/middleware/state')
 
 new koa()
-    .use(state)
-    .use(compress({
-        filter(contentType) {
-            return /text/i.test(contentType)
-        },
-        threshold: 2048,
-        gzip: {
-            flush: require('zlib').constants.Z_SYNC_FLUSH
-        },
-        deflate: {
-            flush: require('zlib').constants.Z_SYNC_FLUSH,
-        },
-        br: false
-    }))
-    .use(mount('/static', serve('app/static', {maxage: 600 * 1000})))
-    .use(views(config.template.path, config.template.options))
-    .use(router.routes())
-    .listen(3000)
+  .use(state)
+  .use(compress({
+    filter(contentType) {
+      return /text/i.test(contentType)
+    },
+    threshold: 2048,
+    gzip: {
+      flush: require('zlib').constants.Z_SYNC_FLUSH
+    },
+    deflate: {
+      flush: require('zlib').constants.Z_SYNC_FLUSH,
+    },
+    br: false
+  }))
+  .use(mount('/static', serve('app/static', {maxage: 600 * 1000})))
+  .use(views(config.template.path, config.template.options))
+  .use(router.routes())
+  .listen(config.get('server.port'))
