dbd0ca151307006d9cd9a344030cd5340faf62e3
董阔 <dongkuo@vzan.com>
2022-02-20 17:58:31 +0800

    修改样式

--- db/helper/test/md2htmlTest.js
+++ db/helper/test/md2htmlTest.js
@@ -1,19 +1,8 @@
 const md2html = require('../md2html')
 const fs = require('fs')
 
-let data = fs.readFileSync('./test.md', 'utf-8')
+let data = fs.readFileSync('/Users/derker/Documents/博客/tidy/coding/2017-04-15-自己动手实现DNS协议.md', 'utf-8')
 
 let output = md2html(data)
 
-output = `
-<!doctype html>
-<html lang="zh">
-<head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css">
-</head>
-<body>
-${output}
-</body>
-</html>
-`
 fs.writeFileSync('./md.html', output, "utf-8")
